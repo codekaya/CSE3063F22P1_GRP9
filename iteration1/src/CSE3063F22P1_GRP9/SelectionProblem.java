57c1ab42b2895773cb06966ef1762b6fb042440b
Emir Yılmaz <53827052+ULUvi@users.noreply.github.com>
2022-11-11 20:19:15 +0300

    Add files via upload
    SelectionProblem class added

---
+++ iteration1/src/CSE3063F22P1_GRP9/SelectionProblem.java
@@ -0,0 +1,27 @@
+package CSE3063F22P1_GRP9;
+
+public class SelectionProblem {
+	private int Id;
+	private Course notRegisteredCourse;
+	private String description;
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public Course getNotRegisteredCourse() {
+		return notRegisteredCourse;
+	}
+	public void setNotRegisteredCourse(Course notRegisteredCourse) {
+		this.notRegisteredCourse = notRegisteredCourse;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
