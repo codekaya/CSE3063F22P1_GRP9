c4d28c2961f2c5b49e56d104ded9db36036c6727
Emir Yılmaz <53827052+ULUvi@users.noreply.github.com>
2023-01-01 17:30:35 +0300

    SelectionProblem Added

--- iteration3/src/SelectionProblem.py
+++ iteration3/src/SelectionProblem.py
@@ -1,14 +1,19 @@
 class SelectionProblem:
-    def __init__(self, not_registered_course, Id=None, description=None):
-        self.Id = Id
+    def __init__(self, not_registered_course):
         self.not_registered_course = not_registered_course
+        self.id = None
+        self.description = None
+
+    def __init__(self, id, not_registered_course, description):
+        self.not_registered_course = not_registered_course
+        self.id = id
         self.description = description
-        
+
     def get_id(self):
-        return self.Id
-    
+        return self.id
+
     def get_not_registered_course(self):
         return self.not_registered_course
-    
+
     def get_description(self):
         return self.description
