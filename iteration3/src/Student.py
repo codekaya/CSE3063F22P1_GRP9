b4e8c46f4f7f3b4b4b9ec52e6d81384d6fc6702f
Muslim Yilmaz <mslmyilmaz5@gmail.com>
2023-01-06 21:40:15 +0300

    student selectCourses method added

--- iteration3/src/Student.py
+++ iteration3/src/Student.py
@@ -1,22 +1,27 @@
 from Person import Person
 from TakenCourse import TakenCourse
 from SelectionProblem import SelectionProblem
-import json
+import logging
 class Student(Person):
     def __init__(self, Id: str, FName: str, LName: str, semester: int, advisor):
         super().__init__(Id, FName, LName)
-        self.semester = semester
-        self.advisor = advisor
-        self.requested_courses = []
-        self.transcript = None
+        self._semester = semester
+        self._advisor = advisor
+        self._requested_courses = []
+        self._transcript = None
         self._weeklySchedule = []
+        self._logger = logging.getLogger(__name__)
 
     def scheduleCourse(self,course):
         for schedule in course.getScheduleList():
             for studentSchedule in self._weeklySchedule:
                 if schedule.checkCollision(studentSchedule[0]):
                     description = "Couldn't registered it collides with " + studentSchedule[1].getName()
-                    self.transcript.addSelectionProblem(SelectionProblem(4,course,description))
+                    course.getCourseStatistics().incrRegistrationFailureCount()
+                    course.getCourseStatistics().incrCollisionProblemCount()
+                    self._logger.warning(f' Registration failed: Student with id {self.getID()} can not take {course.getName()} because it collides with {studentSchedule[1].getName()}')
+                    self._logger.info(f'\t\t ■  Registration failure count updated for course {course.getName()} --> Problem count:{course.getCourseStatistics().getCollisionProblemCount()}')
+                    self._transcript.addSelectionProblem(SelectionProblem(4,course,description))
                     return False #There is a collision
         for schedule in course.getScheduleList():
             self._weeklySchedule.append([schedule,course])
@@ -25,31 +30,45 @@ class Student(Person):
     def registerToCourse(self,course):
         if self.scheduleCourse(course):
             takenCourse = TakenCourse(course,0,"Current")
-            self.transcript.addTakenCourse(takenCourse)
+            self._transcript.addTakenCourse(takenCourse)
             return True #Student has registered course successfully
         return False #Registration failed because of collision problem
+
+    def selectCourses(self,courses):
+        for takenCourse in self._transcript.getTakenCourses():
+            if takenCourse.getTakenCourseStatus() == 'Failed':
+                self._requested_courses.append(takenCourse.getCourse())
+        for selectionProblem in self._transcript.getSelectionProblems():
+            notRegisteredCourse = selectionProblem.get_not_registered_course()
+            self._requested_courses.append(notRegisteredCourse)
+        self._transcript.getSelectionProblems().clear()
+        for course in courses[self._semester-1]:
+            self._requested_courses.append(course)
+        self._logger.info(f'◉ Random sutdent with id:{self.getID()},semester:{self._semester + 1} and requested courses list:' )
+        for course in self.getRequestedCourses():
+            self._logger.info(f'\t{course.getName()} → Quota:{course.getQuota()}' )
+            if course.getPrerequisite() is not None:
+             self._logger.info(f'\t   ■  Prerequiste(s):{course.getPrerequisite().getName()}')
         
     def getSemester(self):
-        return self.semester
+        return self._semester
     
     def getTranscript(self):
-        return self.transcript
+        return self._transcript
     
     def getAdvisor(self):
-        return self.advisor
+        return self._advisor
     
     def getRequestedCourses(self):
-        return self.requested_courses
+        return self._requested_courses
     
     def addRequestedCourse(self, course):
-        self.requested_courses.append(course)
+        self._requested_courses.append(course)
         
     def setTranscript(self, transcript):
-        self.transcript = transcript
+        self._transcript = transcript
         
     def status(self):
-        print(f"Student in university with id {self.get_ID()} and semester {self.semester}")
+        print(f"Student in university with id {self.getID()} and semester {self._semester}")
     
-    def toJson(self):
-        return json.dumps(self, default=lambda o: o.__dict__)
 
