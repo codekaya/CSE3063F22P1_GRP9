9438fd47d6a1a9d7409dd2d5b3dc894c3991dc3f
Muhammed Mustafa İnce <86130203+lolndrug@users.noreply.github.com>
2023-01-07 11:25:45 +0300

    collision and max_course statistics added.

--- iteration3/src/CourseStatistics.py
+++ iteration3/src/CourseStatistics.py
@@ -1,34 +1,48 @@
 class CourseStatistics:
     
     def __init__(self):
-        self.registeredStudentCount = 0
-        self.registrationFailureCount = 0
-        self.quotaProblemCount = 0
-        self.prerequisiteProblemCount = 0
+        self._registeredStudentCount = 0
+        self._registrationFailureCount = 0
+        self._quotaProblemCount = 0
+        self._prerequisiteProblemCount = 0
+        self._collisionProblemCount = 0
+        self._maxcourseProblemCount = 0
 
     def incrRegisteredStudentCount(self):
-        self.registeredStudentCount += 1
+        self._registeredStudentCount += 1
 
     def incrRegistrationFailureCount(self):
-        self.registrationFailureCount += 1
+        self._registrationFailureCount += 1
 
     def incrQuotaProblemCount(self):
-        self.quotaProblemCount += 1
+        self._quotaProblemCount += 1
 
     def incrPrerequisiteProblemCount(self):
-        self.prerequisiteProblemCount += 1
+        self._prerequisiteProblemCount += 1
+    
+    def incrCollisionProblemCount(self):
+        self._collisionProblemCount += 1
+    
+    def incrMaxcourseProblemCount(self):
+        self._maxcourseProblemCount +=1
 
     def getRegisteredStudentCount(self):
-        return self.registeredStudentCount
+        return self._registeredStudentCount
 
     def getRegistrationFailureCount(self):
-        return self.registrationFailureCount
+        return self._registrationFailureCount
 
     def getQuotaProblemCount(self):
-        return self.quotaProblemCount
+        return self._quotaProblemCount
 
     def getPrerequisiteProblemCount(self):
-        return self.prerequisiteProblemCount
+        return self._prerequisiteProblemCount
+    
+    def getCollisionProblemCount(self):
+        return self._collisionProblemCount
 
+    def getMaxcourseProblemCount(self):
+        return self._maxcourseProblemCount
+        
     def getRateOfSuccessfulRegistration(self):
-        return self.registeredStudentCount / (self.registrationFailureCount + self.registeredStudentCount)
+        return self._registeredStudentCount / (self._registrationFailureCount + self._registeredStudentCount)
