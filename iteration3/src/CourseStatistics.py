137257da7a6750259e5aba4a0bddfb6ae94532d3
Evren Koymat <105942580+evrenkymt@users.noreply.github.com>
2023-01-07 22:32:15 +0300

    str method added

--- iteration3/src/CourseStatistics.py
+++ iteration3/src/CourseStatistics.py
@@ -1,6 +1,7 @@
 class CourseStatistics:
     
-    def __init__(self):
+    def __init__(self,course):
+        self._course = course
         self._registeredStudentCount = 0
         self._registrationFailureCount = 0
         self._quotaProblemCount = 0
@@ -8,6 +9,17 @@ class CourseStatistics:
         self._collisionProblemCount = 0
         self._maxcourseProblemCount = 0
 
+    def __str__(self):
+        s = f' -->  {self._course.getID().upper()} {self._course.getName().upper()}\n\n'
+        s += "\t■ Number of students applied for course :"+str(self.getRegisteredStudentCount() + self.getRegistrationFailureCount())+"\n"
+        s += "\t■ Number of registered students:"+str(self.getRegisteredStudentCount())+"\n"
+        s += "\t■ Number of registration failures:"+str(self.getRegistrationFailureCount())+"\n"
+        s += "\t - Number of quota problems:"+str(self.getQuotaProblemCount())+"\n"
+        s += "\t - Number of prerequisite problems:"+str(self.getPrerequisiteProblemCount())+"\n"
+        s += "\t - Number of collision problems:"+str(self.getCollisionProblemCount())+"\n"
+        s += "\t - Number of max limit course problems:"+str(self.getMaxcourseProblemCount())+"\n\n"
+        return s
+        
     def incrRegisteredStudentCount(self):
         self._registeredStudentCount += 1
 
@@ -43,6 +55,4 @@ class CourseStatistics:
 
     def getMaxcourseProblemCount(self):
         return self._maxcourseProblemCount
-        
-    def getRateOfSuccessfulRegistration(self):
-        return self._registeredStudentCount / (self._registrationFailureCount + self._registeredStudentCount)
+
