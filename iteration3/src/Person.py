87ce917f559da803638afcc3423bbffe13ab896f
SinanGo <61915608+SinanGo@users.noreply.github.com>
2023-01-09 04:41:36 +0300

    privacy changes are made for Person.py

--- iteration3/src/Person.py
+++ iteration3/src/Person.py
@@ -3,19 +3,19 @@ from abc import ABC, abstractclassmethod
 
 class Person(ABC):
     def __init__(self, ID, first_name, last_name):
-        self.ID = ID
-        self.first_name = first_name
-        self.last_name = last_name
+        self._ID = ID
+        self._first_name = first_name
+        self._last_name = last_name
 
     @abstractclassmethod   
     def status(self):
         pass
         
     def getID(self):
-        return self.ID
+        return self._ID
     
     def getFirstName(self):
-        return self.first_name
+        return self._first_name
     
     def getLastName(self):
-        return self.last_name
+        return self._last_name
